c80e1be13a1cb9386b31d87ed0fc9df42b98110f
mfrancis <margarida.matiass@gmail.com>
2025-11-06 10:54:21 +0000

    ex01 still in progress

---
+++ CPP08/ex01/Span.cpp
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfrancis <mfrancis@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 18:34:38 by mfrancis          #+#    #+#             */
+/*   Updated: 2025/11/06 10:51:41 by mfrancis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+Span::Span(unsigned int n):vSize(n)
+{
+    std::cout << "Construter Called" << std::endl;
+}
+
+Span::Span(const Span &copy): vSize(copy.vSize), storage(copy.storage)
+{
+    std::cout << "Copy Construter Called" << std::endl;
+}
+
+Span &Span::operator=(const Span &copy)
+{
+    if(this != &copy)
+    {
+        vSize = copy.vSize;
+        storage = copy.storage;
+    }
+    std::cout << "Copy Assigment Construter Called" << std::endl;
+    return(*this);
+}
+Span::~Span()
+{
+    std::cout << "Destruter Called" << std::endl;
+}
+
+void Span::addNumber(int nbr)
+{
+    if(storage.size() >= vSize)
+        throw std::runtime_error("Span is full!");
+    storage.push_back(nbr);
+}
+
+int Span::shortestSpan() const
+{
+    if(storage.size() <= 1)
+        throw std::runtime_error("Not enougth elements!");
+    
+    std::vector<int> copy = storage;
+
+    std::sort(copy.begin(), copy.end());
+
+    int small = std::numeric_limits<int>::max();
+    for(int i = 0; i < copy.size(); i ++)
+    {
+        int diff = copy[i] - copy[i + 1];
+        if(diff < small)
+            small = diff;
+    }
+    return (small);
+}
+
+int Span::longestSpan() const
+{
+    if(storage.size() <= 1)
+        throw std::runtime_error("Not enougth elements!");
+    
+    int minNbr = std::min_element(storage.begin(), storage.end());
+    int maxNbr = std::max_element(storage.begin(), storage.end());
+
+    return(maxNbr - minNbr);
+}
+   
+
+void Span::addMultipleNumber(std::vector<int> storage)
+{
+    
+}
+    
+
+void Span::printNbrs() const
+{
+    for(int i = 0; i < storage.size(); i++)
+    {
+        std::cout << storage[i] << std::endl;
+    }
+}
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -6,21 +6,36 @@
 /*   By: mfrancis <mfrancis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 17:29:39 by mfrancis          #+#    #+#             */
-/*   Updated: 2025/11/04 17:32:11 by mfrancis         ###   ########.fr       */
+/*   Updated: 2025/11/06 10:45:21 by mfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <exception>
+
 class Span
 {
     private:
+        std::vector<int> storage;
+        unsigned int vSize;
    
     public:
-    addNumber();
-    shortestSpan();
-    longestSpan();
+    Span(unsigned int n);
+    Span(const Span &copy);
+    Span &operator=(const Span &copy);
+    ~Span();
     
-}
+    void addNumber(int nbr);
+    int shortestSpan() const;
+    int longestSpan() const;
+
+    void addMultipleNumber(std::vector<int> storage);
+    void printNbrs() const;
+};
+
 #endif
